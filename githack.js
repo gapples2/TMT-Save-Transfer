1673a7bf79c08c46ae5671b41bbc89ef0b96acec
Glitch (cross-storage-testing) <none>
2022-02-12 21:30:53 +0000

    🛏️🌾 Updated with Glitch

---
+++ githack.js
@@ -0,0 +1,17 @@
+const k = document.getElementById("k")
+
+window.onload = function() {
+  var receiver = k.contentWindow;
+  
+  function receiveMessage(e) {
+    if (e.origin !== "https://gapples2.github.io/TMT-Save-Transfer/")
+      return;
+
+    sendMessage()
+  }
+  
+  function sendMessage(e) {
+    receiver.postMessage(JSON.stringify(localStorage), 'https://gapples2.github.io/TMT-Save-Transfer/');
+  }
+  window.addEventListener('message', receiveMessage);
+}
---
+++ script.js
@@ -0,0 +1,22 @@
+const k = document.getElementById("k")
+
+window.onload = function() {
+  var receiver = k.contentWindow;
+  var messageEle = document.getElementById('message');
+  var btn = document.getElementById('send');
+  
+  function receiveMessage(e) {
+    if (e.origin !== "https://raw.githack.com/gapples2/TMT-Save-Transfer/glitch/githack.html")
+      return;
+
+    console.log(JSON.parse(e.data))
+  }
+  
+  function sendMessage(e) {
+    e.preventDefault();
+
+    receiver.postMessage('storage please', 'https://raw.githack.com/gapples2/TMT-Save-Transfer/glitch/githack.html');
+  }
+  btn.addEventListener('click', sendMessage);
+  window.addEventListener('message', receiveMessage);
+}
