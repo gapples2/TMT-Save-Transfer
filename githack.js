d7aa0bae1d839679d3ed95ce1bf6ffca97c5598e
Glitch (cross-storage-testing) <none>
2022-02-14 11:32:34 +0000

    ⚫📬 Updated with Glitch

--- githack.js
+++ githack.js
@@ -1,13 +1,9 @@
 let l = "https://gapples2.github.io/TMT-Save-Transfer/"
 
 const k = document.getElementById("k")
-
-  var receiver = k.contentWindow;
-function receiveMessage(e) {
-    sendMessage()
-  }
-  
-  function sendMessage(e) {
-    receiver.postMessage(JSON.stringify(localStorage), l);
-  }
-  window.addEventListener('message', receiveMessage);
+const btn=document.getElementById("send")
+var receiver = k.contentWindow;
+function sendMessage(e) {
+  receiver.postMessage(JSON.stringify(localStorage), l);
+}
+btn.addEventListener('click', sendMessage);
