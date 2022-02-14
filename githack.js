1e4cedbaeeee281a7585f30ebb753269afb90462
gapples2 <71836730+gapples2@users.noreply.github.com>
2022-02-14 06:37:10 -0500

    Update githack.js

--- githack.js
+++ githack.js
@@ -1,4 +1,4 @@
-let l = "https://gapples2.github.io/TMT-Save-Transfer/"
+let l = "https://rawcdn.githack.com/gapples2/TMT-Save-Transfer/4332021f3465f61237bc6180bb0a0045b8316b44/get.html"
 
 const k = document.getElementById("k")
 const btn=document.getElementById("send")
